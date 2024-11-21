64b3e720eb8708f34a9ffeefa787f3adeb020dcb
Fabricio C Zuardi <fabricio@fabricio.org>
2024-11-21 03:09:58 -0300

    fix typo

--- .distrobox/make-container.sh
+++ .distrobox/make-container.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 podman build -t bdl -f oci/dev.Containerfile
-BX_CONTAINER_ALWAYS_PULL=0 distrobox create --image bdl --name bdl
+DBX_CONTAINER_ALWAYS_PULL=0 distrobox create --image bdl --name bdl
